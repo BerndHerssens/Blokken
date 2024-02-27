13abc8a024337b730467ff8a91e00771ad2ce20b
Illya Verheyden <illya.verheyden94@gmail.com>
2024-02-27 11:22:48 +0100

    Added Winrates in PlayerPlus

--- Groepsproject_Blokken/PlayerPlus.cs
+++ Groepsproject_Blokken/PlayerPlus.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+//Todo nog niks :)
 namespace Groepsproject_Blokken
 {
     public partial class Player
     {
+        //Als je beide values tegelijk wilt met text erbij
+        public string CalculateWinRates() //Geeft beide winrates
+        {
+            return "Winrate in SP: " + SPGamesWon / SPGamesPlayed * 100 + " %" + Environment.NewLine + "Winrate in VS: " + VSGamesWon / VSGamesPlayed + " %";
+        }
+        // Als je het in een txtvak wilt gooien en je het wilt opsplitsen :)
+        public string CalculateSPWinRate()
+        {
+            var WinrateSP = Convert.ToString(SPGamesWon / SPGamesPlayed * 100) + "%";
+            return WinrateSP;
+        }
+        public string CalculateVSWinRate()
+        {
+            var WinrateVS = Convert.ToString(VSGamesWon / VSGamesPlayed * 100) + "%";
+            return WinrateVS;
+        }
     }
+}
 }
