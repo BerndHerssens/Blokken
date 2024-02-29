f68206c22d2a26c65b20f1eea8b0ceb696d75b8e
Illya Verheyden <illya.verheyden94@gmail.com>
2024-02-29 11:18:47 +0100

    PlayerPlus Bernd Implementation, cleanup of Gitignore, delete exe and pdb file

--- Groepsproject_Blokken/PlayerPlus.cs
+++ Groepsproject_Blokken/PlayerPlus.cs
@@ -7,17 +7,31 @@ namespace Groepsproject_Blokken
         //Als je beide values tegelijk wilt met text erbij
         public string CalculateWinRates() //Geeft beide winrates
         {
-            return "Winrate in SP: " + SPGamesWon / SPGamesPlayed * 100 + " %" + Environment.NewLine + "Winrate in VS: " + VSGamesWon / VSGamesPlayed + " %";
+            var WinrateSPVS = "Winrate in SP: " + SPGamesWon / SPGamesPlayed * 100 + " %" + Environment.NewLine + "Winrate in VS: " + VSGamesWon / VSGamesPlayed + " %";
+            if (this.SPGamesPlayed == null || this.VSGamesPlayed == null)  //Als een van deze null zijn dan geven we N/A mee met deze functie
+            {
+                WinrateSPVS = "N/A";
+            }
+
+            return WinrateSPVS; //Gooit regel 10 indien het niet null is, regel 13 als een van de twee null is.
         }
         // Als je het in een txtvak wilt gooien en je het wilt opsplitsen :)
-        public string CalculateSPWinRate()
+        public string CalculateSPWinRate() //Zelfde logica dan bij functie CalculateWinRates();
         {
             var WinrateSP = Convert.ToString(SPGamesWon / SPGamesPlayed * 100) + "%";
+            if (this.SPGamesPlayed == null)
+            {
+                WinrateSP = "N/A";
+            }
             return WinrateSP;
         }
         public string CalculateVSWinRate()
         {
             var WinrateVS = Convert.ToString(VSGamesWon / VSGamesPlayed * 100) + "%";
+            if (this.VSGamesPlayed == null)
+            {
+                WinrateVS = "N/A";
+            }
             return WinrateVS;
         }
     }
