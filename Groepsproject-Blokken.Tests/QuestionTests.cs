2b6fb6f0b10f4437de6f2de80d34886d94f4ae3f
Illya Verheyden <illya.verheyden94@gmail.com>
2024-03-01 16:59:33 +0100

    bugfix aanpassing

--- Groepsproject-Blokken.Tests/QuestionTests.cs
+++ Groepsproject-Blokken.Tests/QuestionTests.cs
@@ -6,7 +6,7 @@
         public void QuestionTest()
         {
             //Arrange
-            Question vraag1 = new Question("Welke 2 landen werden zwaar getroffen door een aardbeving in februari?", "Turkije en Syrië", "Japan en Hawaï", "Bulgarije en Griekenland", "België en Nederland");
+            // Question vraag1 = new Question("Welke 2 landen werden zwaar getroffen door een aardbeving in februari?", "Turkije en Syrië", "Japan en Hawaï", "Bulgarije en Griekenland", "België en Nederland");
             //TOdo :Wat kan ik hier testen?
 
             //Act
