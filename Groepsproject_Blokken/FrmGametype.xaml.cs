3fa19ed53961e4dd0934b2518b09ddb3ad1f1299
bernd_herssens <berndherssens@hotmail.com>
2024-03-07 13:57:12 +0100

    Primeword afgewerkt, en in de Gametype gestoken

--- Groepsproject_Blokken/FrmGametype.xaml.cs
+++ Groepsproject_Blokken/FrmGametype.xaml.cs
@@ -16,14 +16,30 @@ namespace Groepsproject_Blokken
         {
             InitializeComponent();
         }
+        // deze heb ik nodig voor de Primewords te laden en weg te schrijven
+        PrimeWord myPrimeWord;
+        string[] temperaryLines;
+        string[,] wordsThatAreParted;
+        int teller = 0;
+        //deze is voor in de game vensters
+        char[] wordForDisplay = "________".ToCharArray(); //dit is wat we tonen op het scherm
+        char[] versnipperdPrimeWord = new char[8]; //dit is het myPrimeWord.Primeword waar we mee gaan werken
+        //myPrimeword.Hint is je hint dat je kan tonen
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            LaadTXTinCMB();
+            try
+            {
+                LaadTXTinCMB();
+                ReadAndFillPrimeWordList();
+                RandomPrimeWordConstruction();
+            }
+            catch 
+            {
+            
+            }
         }
         List<string> listIngeladenActieveVragenlijsten; //Txt's
         List<string> listGekozenVragenlijsten = new List<string>(); //*Leeg -> dit gaan we de selectie van de gebruiker inladen
-
-
         private void btnSingle_Click(object sender, RoutedEventArgs e)
         {
             BerndCrabbeWeg.Completed += (s, args) =>
@@ -80,7 +96,6 @@ namespace Groepsproject_Blokken
         {
 
         }
-
         private void sliderVolume_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
             sliderVolume.Opacity = 1;
@@ -131,6 +146,143 @@ namespace Groepsproject_Blokken
                 lbQuestionsDisplay.ItemsSource = null;
                 lbQuestionsDisplay.ItemsSource = listGekozenVragenlijsten;
 
+            }
+        }
+        public void ReadAndFillPrimeWordList() //i want to make a method that will fill in a 2d array of (prime)words, read from a txt
+        {
+            var filepath = "../../PrimeWords/List1.txt";
+            try
+            {
+                StreamReader readobject = new StreamReader(filepath);
+                while (!readobject.EndOfStream)
+                {
+                    readobject.ReadLine();
+                    if (readobject.ToString() == null || readobject.ToString() == "") //this is when txt1 is empty
+                    {
+                        File.Copy("../../PrimeWords/List2.txt", "../../PrimeWords/List1.txt");
+                        System.IO.File.WriteAllText("../../PrimeWords/file2.txt", string.Empty);
+                    }
+                    else //here i just need the lenght of the list, so i need to increase "teller"
+                    {
+                        teller++;
+                    }
+                }
+                readobject.Close();
+                StreamReader readobject2 = new StreamReader(filepath);
+                while (!readobject2.EndOfStream)
+                {
+                    temperaryLines = new string[teller]; //now that i have teller, i can make my length
+                    for (int i = 0; i < teller; i++)
+                    {
+                        temperaryLines[i] = readobject2.ReadLine();
+                    }
+                }
+                readobject2.Close();
+                wordsThatAreParted = new string[teller, 2];
+                for (int i = 0; i < teller; i++)
+                {
+                    string[] parts = temperaryLines[i].Split(';');
+                    if (parts.Length >= 2)
+                    {
+                        wordsThatAreParted[i, 0] = parts[0];  // Left part this will be the primeword then
+                        wordsThatAreParted[i, 1] = parts[1];  // Right part this will be the hint
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("File not found. Please check the file path", "Error");
+            }
+        }
+        public void RandomPrimeWordConstruction()
+        {
+            Random myRandom = new Random();
+            
+            var randomNumberToPickAndRemove = myRandom.Next(0, teller);
+            myPrimeWord = new PrimeWord((wordsThatAreParted[randomNumberToPickAndRemove, 0]), (wordsThatAreParted[randomNumberToPickAndRemove, 1])); //here we have our random prime word with the random Hint
+            string[,] writeAwayString = new string[teller - 1, 2]; //TODO: this one we can move to main, so we can get it for the writing for the new TXT
+            for (int i = 0, j = 0; i < teller; i++) //ik wil hier een 2d string opvullen zonder het gekozen woord
+            {
+                if (i == randomNumberToPickAndRemove)
+                    continue;
+
+                for (int k = 0, u = 0; k < wordsThatAreParted.GetLength(1); k++)
+                {
+                    if (k == randomNumberToPickAndRemove)
+                        continue;
+
+                    writeAwayString[j, u] = wordsThatAreParted[i, k];
+                    u++;
+                }
+                j++;
+            }
+            WriteAwayToTxtOne(writeAwayString);
+            WriteAwayToTxtTwo(myPrimeWord);
+        }
+        public void WriteAwayToTxtOne(string[,] writeAwayString) //overschrijven List1 maar dan zonder de gekozen Primeword
+        {
+            var filepath = "../../PrimeWords/List1.txt";
+            using (StreamWriter writer = new StreamWriter(filepath))
+            {
+                for (int i = 0; i < writeAwayString.GetLength(0); i++) //we write away first the lines that we aleady can read
+                {
+                    writer.WriteLine(writeAwayString[i, 0] + ";" + writeAwayString[i, 1]);
+                }
+            }
+        }
+        public void WriteAwayToTxtTwo(PrimeWord wegschrijvenWoord) //ik heb de Primeword nodig
+        {
+            var filepath = "../../PrimeWords/List2.txt";
+            string[] toWriteAway;
+            int textLenght = 0;
+            StreamReader readobject = new StreamReader(filepath);
+            string temperaryLines;
+            while (!readobject.EndOfStream)
+            {
+                readobject.ReadLine();
+                textLenght++;
+            }
+            readobject.Close();
+            StreamReader readobject2 = new StreamReader(filepath);
+            toWriteAway = new string[textLenght];
+            while (!readobject2.EndOfStream)
+            {
+                for (int i = 0; i < textLenght; i++)
+                {
+                    toWriteAway[i] = readobject2.ReadLine();
+                }
+            }
+            readobject2.Close();
+            using (StreamWriter writer = new StreamWriter(filepath))
+            {
+                for (int i = 0; i < textLenght; i++)
+                {
+                    writer.WriteLine(toWriteAway[i]);
+                }
+                temperaryLines = wegschrijvenWoord.Primeword + ";" + wegschrijvenWoord.Hint;
+                writer.WriteLine(temperaryLines); //we write away the newly used primeword
+            }
+        }
+        //dit is de methode om een primeword te verwerken die zou moeten opgeroepen worden elke X seconden, en dan een letter meer zou moeten terug geven
+        //deze verwerken de 2 objecten boven aan de page
+        public void PrimeWordCuttingAndShowing()
+        {
+            Random myRandom = new Random();
+            int randomInt;
+            bool reroll = true;
+            randomInt = myRandom.Next(0, 8);
+            while (reroll)
+            {
+                if (!(versnipperdPrimeWord[randomInt] == '_'))
+                {
+                    reroll = false;
+                    wordForDisplay[randomInt] = versnipperdPrimeWord[randomInt];
+                    versnipperdPrimeWord[randomInt] = '_';
+                }
+                else
+                {
+                    randomInt = myRandom.Next(0, 8);
+                }
             }
         }
     }
